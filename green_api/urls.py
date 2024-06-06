51b657e4e4b9d0efd73d13b27b367f8a62c4e0f1
Artem <thaimedved@gmail.com>
2024-06-07 07:21:06 +0800

    new page

--- green_api/urls.py
+++ green_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, index2
 
 
 urlpatterns = [
     path('', index, name='home'),
+    path('new_way/', index2, name='home2'),
 ]
--- green_api/views.py
+++ green_api/views.py
@@ -8,8 +8,8 @@ title = "GREEN-API"
 
 
     
-def make_requests(request, method, url, payload, headers):
-
+def make_requests(request, method, url, payload, headers, ind):
+    print(ind)
     response = requests.request(method, url, headers=headers, data = payload)
 
     if response.status_code == 200:
@@ -24,7 +24,11 @@ def make_requests(request, method, url, payload, headers):
         'title': title,
         'data': data
     }
-    return render(request, 'index.html', context)
+    if ind == 1:
+        temlate = 'index.html'
+    else:
+        temlate = 'new_way.html'
+    return render(request, temlate, context)
 
 def index(request):
     if request.method == 'POST':
@@ -39,13 +43,13 @@ def index(request):
         if action == 'getSettings':
 
             method = "GET"
-            return make_requests(request, method, url, payload = {}, headers= {})
+            return make_requests(request, method, url, payload = {}, headers= {}, ind = 1)
         
 
         elif action == 'getStateInstance':
 
             method = "GET"
-            return make_requests(request, method, url, payload = {}, headers= {})
+            return make_requests(request, method, url, payload = {}, headers= {}, ind = 1)
         
         
         elif action == 'sendMessage':
@@ -61,7 +65,7 @@ def index(request):
                 'Content-Type': 'application/json'
             }
 
-            return make_requests(request, method, url, payload, headers)
+            return make_requests(request, method, url, payload, headers, ind = 1)
         
 
         elif action == 'sendFileByUrl':
@@ -77,9 +81,69 @@ def index(request):
                 'Content-Type': 'application/json'
             }
 
-            return make_requests(request, method, url, payload, headers)
+            return make_requests(request, method, url, payload, headers, ind = 1)
 
     context = {
         'title': title,
     }
     return render(request, 'index.html', context)
+
+def index2(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+
+        idInstance = request.POST.get('idInstance')
+        apiTokenInstance = request.POST.get('ApiTokenInstance')
+
+        apiUrl = request.POST.get('apiUrl') + '/waInstance'
+
+        url = f"{apiUrl}{idInstance}/{action}/{apiTokenInstance}"
+
+
+        if action == 'getSettings':
+
+            method = "GET"
+            return make_requests(request, method, url, payload = {}, headers= {}, ind = 2)
+        
+
+        elif action == 'getStateInstance':
+
+            method = "GET"
+            return make_requests(request, method, url, payload = {}, headers= {}, ind = 2)
+        
+        
+        elif action == 'sendMessage':
+
+            method = "POST"
+
+            chat_id = request.POST.get('chatIdSendMessage')
+            mes = request.POST.get('message')
+
+            payload = "{\r\n\t\"chatId\": \"%s\",\r\n\t\"message\": \"%s\"\r\n}" % (chat_id, mes)
+
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            return make_requests(request, method, url, payload, headers, ind = 2)
+        
+
+        elif action == 'sendFileByUrl':
+
+            method = "POST"
+
+            chat_id = request.POST.get('chatIdSendFileByUrl')
+            mes = request.POST.get('urlFile')
+            
+            payload = "{\r\n   \t\"chatId\": \"%s\",\r\n\t\"urlFile\": \"%s\",\r\n\t\"fileName\": \"%s\"\r\n}" % (chat_id, mes, mes)
+
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            return make_requests(request, method, url, payload, headers, ind = 2)
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'new_way.html', context)
